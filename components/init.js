1cdad6a7c48e1ed6b1e35817553688a45feedf31
Stepbackru <alex-ruba@bk.ru>
2020-09-23 22:32:45 +0300

    chore: remove comments from init

--- components/init.js
+++ components/init.js
@@ -6,16 +6,7 @@ const init = () => {
 }
 
 const initTemplate = () => {
-  template.forEach(el => {
-    // if (!el.dataAttr) {
-    //   creator(el.el, el.className, el.child, el.parent);
-    //   // creator(el.el, el.className, el.child, el.parent);
-    // }
-    // if (el.dataAttr) {
-    //   creator(el.el, el.className, el.child, el.parent, ...el.dataAttr);
-    // }
-    creator(el);
-  })
+  template.forEach(el => creator(el));
 }
 
 export default init;
