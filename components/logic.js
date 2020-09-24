7f4f791d90ac1b4ebaf077d6f14f8bb398e8f72f
Stepbackru <alex-ruba@bk.ru>
2020-09-25 00:36:21 +0300

    feat: add logic of calc

---
+++ components/logic.js
@@ -0,0 +1,57 @@
+const logic = () => {
+  const output = document.querySelector('.display__input');
+  let valueMemory = null;
+  let value2 = null;
+  let operationMemory = '';
+
+  document.querySelector('.wrapper').addEventListener('click', (e) => {
+    const item = e.target;
+    
+    if (item.hasAttribute('data-number')) {
+      if (output.value.length < 13) {
+        output.value += item.getAttribute('data-number');
+        if (valueMemory !== null) {
+          output.value = item.getAttribute('data-number');
+        }
+      }
+    }
+    
+    if (output.value === '.' && valueMemory === null) {
+      let addZero = output.value.split('');
+      addZero.unshift('0');
+      output.value = addZero.join('');
+    }
+
+    if (item.hasAttribute('data-operation')) {
+      valueMemory = Number(output.value);
+      operationMemory = item.getAttribute('data-operation');
+    }
+
+    if (item.hasAttribute('data-action')) {
+      if (item.getAttribute('data-action') === 'clear') {
+        valueMemory = null;
+        output.value = '';
+      }
+      if (item.getAttribute('data-action') === 'answer') {
+        if (valueMemory !== null) {
+          value2 = Number(output.value);
+          if (operationMemory === '+') {
+            output.value = valueMemory + value2;
+          }
+          if (operationMemory === '-') {
+            output.value = valueMemory - value2;
+          }
+          if (operationMemory === '/') {
+            output.value = (valueMemory / value2);
+          }
+          if (operationMemory === '*') {
+            output.value = valueMemory * value2;
+          }
+          
+        }
+      }
+    }
+  });
+}
+
+export default logic;
