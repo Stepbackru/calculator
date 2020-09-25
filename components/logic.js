22f396696b7b6967c40328762448fff91c961d40
Stepbackru <alex-ruba@bk.ru>
2020-09-25 21:43:16 +0300

    refactor: render numbers to input+logic operations

--- components/logic.js
+++ components/logic.js
@@ -1,6 +1,7 @@
 const logic = () => {
   const output = document.querySelector('.display__input');
   let valueMemory = null;
+  let newValue = false;
   let value2 = null;
   let operationMemory = '';
 
@@ -9,39 +10,46 @@ const logic = () => {
     
     if (item.hasAttribute('data-number')) {
       if (output.value.length < 13) {
-        if (output.value === '0') {
+        if (newValue) {
           output.value = item.getAttribute('data-number');
+          newValue = false;
         } else {
-          output.value += item.getAttribute('data-number');
+          if (output.value === '0') {
+            output.value = item.getAttribute('data-number');
+          } else {
+            output.value += item.getAttribute('data-number');
+          }
         }
       }
     }
 
     if (item.hasAttribute('data-operation')) {
       valueMemory = Number(output.value);
+      newValue = true;
       operationMemory = item.getAttribute('data-operation');
     }
 
     if (item.hasAttribute('data-action')) {
       if (item.getAttribute('data-action') === 'clear') {
         valueMemory = null;
-        output.value = '';
+        newValue = false;
         operationMemory = '';
+        output.value = '0';
       }
       if (item.getAttribute('data-action') === 'answer') {
         if (valueMemory !== null) {
           value2 = Number(output.value);
           if (operationMemory === '+') {
-            output.value = valueMemory + value2;
+            output.value = parseFloat(valueMemory + value2);
           }
           if (operationMemory === '-') {
-            output.value = valueMemory - value2;
+            output.value = parseFloat(valueMemory - value2);
           }
           if (operationMemory === '/') {
-            output.value = (valueMemory / value2);
+            output.value = parseFloat(valueMemory / value2);
           }
           if (operationMemory === '*') {
-            output.value = valueMemory * value2;
+            output.value = parseFloat(valueMemory * value2);
           }
           
         }
